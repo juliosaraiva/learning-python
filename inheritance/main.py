f86c30f39f0034d342803a99830b72de110ad1d8
Julio Saraiva <contato@juliosaraiva.com.br>
2020-11-02 04:34:04 -0300

    :sparkles: Added employees and productivity

--- inheritance/main.py
+++ inheritance/main.py
@@ -1,14 +1,20 @@
-class PayrollSystem:
-    def calculate_payroll(self, employees):
-        print('Calculating Payroll')
-        print('====================')
-        for employee in employees:
-            print(f'Payroll for: {employee.id} - {employee.name}')
-            print(f'- Check amount: {employee.calculate_payroll()}')
-            print('')
+import hr
+import employees
+import productivity
 
+manager = employees.Manager(1, 'Julio Saraiva', 3000)
+secretary = employees.Secretary(2, 'Wirllane Borges', 2000)
+sales_guy = employees.SalesPerson(3, 'Pedro Augusto', 1000, 250)
+factory_worker = employees.FactoryWorker(4, 'Gustavo Borges', 20, 15)
 
-class Employee:
-    def __init__(self. id, name):
-        self.id = id
-        self.name = name
+employees = [
+    manager,
+    secretary,
+    sales_guy,
+    factory_worker,
+]
+
+productivity_system = productivity.ProductivitySystem()
+productivity_system.track(employees, 40)
+payroll_system = hr.PayrollSystem()
+payroll_system.calculate_payroll(employees)
