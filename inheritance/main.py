6adf4de7e7bd899dc8ba7a8a684208373c58d92a
Julio Saraiva <contato@juliosaraiva.com.br>
2020-11-08 17:47:34 -0300

    :recycle: Refactor classes to improve inheritance

--- inheritance/main.py
+++ inheritance/main.py
@@ -5,13 +5,15 @@ import productivity
 manager = employees.Manager(1, 'Julio Saraiva', 3000)
 secretary = employees.Secretary(2, 'Wirllane Borges', 2000)
 sales_guy = employees.SalesPerson(3, 'Pedro Augusto', 1000, 250)
-factory_worker = employees.FactoryWorker(4, 'Gustavo Borges', 20, 15)
+factory_worker = employees.FactoryWorker(4, 'Alura Mac', 20, 15)
+temporary_secretary = employees.TemporarySecretary(5, 'Denisse Stephan', 40, 9)
 
 employees = [
     manager,
     secretary,
     sales_guy,
     factory_worker,
+    temporary_secretary
 ]
 
 productivity_system = productivity.ProductivitySystem()
--- inheritance/productivity.py
+++ inheritance/productivity.py
@@ -5,3 +5,23 @@ class ProductivitySystem:
         for employee in employees:
             employee.work(hours)
         print('')
+
+
+class ManagerRole:
+    def work(self, hours):
+        print(f'{self.name} screams and yells for {hours} hours')
+
+
+class SecretaryRole:
+    def work(self, hours):
+        print(f'{self.name} expends {hours} hours doing office paperwork')
+
+
+class SalesRole:
+    def work(self, hours):
+        print(f'{self.name} expends {hours} hours on the phone')
+
+
+class FactoryRole:
+    def work(self, hours):
+        print(f'{self.name} manufactures gadgets for {hours} hours')
