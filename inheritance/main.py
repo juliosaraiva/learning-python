8c355790659cb012eb0c929c93dc154cd49249de
Julio Saraiva <contato@juliosaraiva.com.br>
2020-11-10 06:57:47 -0300

    finished about composition

--- inheritance/main.py
+++ inheritance/main.py
@@ -1,22 +1,9 @@
-import hr
-import employees
-import productivity
+from hr import PayrollSystem
+from productivity import ProductivitySystem
+from employees import EmployeeDatabase
 
-manager = employees.Manager(1, 'Julio Saraiva', 3000)
-secretary = employees.Secretary(2, 'Wirllane Borges', 2000)
-sales_guy = employees.SalesPerson(3, 'Pedro Augusto', 1000, 250)
-factory_worker = employees.FactoryWorker(4, 'Alura Mac', 20, 15)
-temporary_secretary = employees.TemporarySecretary(5, 'Denisse Stephan', 40, 9)
 
-employees = [
-    manager,
-    secretary,
-    sales_guy,
-    factory_worker,
-    temporary_secretary
-]
-
-productivity_system = productivity.ProductivitySystem()
-productivity_system.track(employees, 40)
-payroll_system = hr.PayrollSystem()
-payroll_system.calculate_payroll(employees)
+employee_database = EmployeeDatabase()
+employees = employee_database.employees
+ProductivitySystem().track(employees, 40)
+PayrollSystem().calculate_payroll(employees)
