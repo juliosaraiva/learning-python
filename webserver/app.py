3c0e5a254075bcbb51b2c563d42cce88be5d37a5
Julio Saraiva <contato@juliosaraiva.com.br>
2020-11-23 07:48:07 -0300

    Developing portifolio

--- webserver/app.py
+++ webserver/app.py
@@ -3,10 +3,5 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
-
-
-@app.route('/blog')
-def my_blog():
-    return 'This is my blog'
