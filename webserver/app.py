86c7fddba4b0ef85277fa141d1032fc7607fe9ff
Julio Saraiva <contato@juliosaraiva.com.br>
2020-11-23 19:26:53 -0300

    Building a portifolio

--- webserver/app.py
+++ webserver/app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+import csv
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+def write_to_file(**kwargs):
+    with open('database.csv', 'a') as db:
+        data = csv.writer(db, delimiter=',')
+        data.writerow(kwargs.values())
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(**data)
+        return data
+    else:
+        return 'Something wrong Happened!'
